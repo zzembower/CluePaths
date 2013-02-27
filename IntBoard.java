cb4ed4ef1656d8a44cf63b11f712cc9277e31250
unknown <zzembowe@.adit.mines.edu>
2013-02-27 16:25:30 -0700

    Add java files

---
+++ IntBoard.java
@@ -0,0 +1,35 @@
+package experiment;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Set;
+
+public class IntBoard {
+
+	public IntBoard() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public void calcAdjacencies() {
+		
+	}
+	
+	public void startTargets(int location, int roll) {
+		
+	}
+	
+	public int calcIndex(int x, int y) {
+		return 0;
+	}
+	
+	public Set getTargets() {
+		return new HashSet();
+	}
+	
+	public LinkedList<Integer> getAdjList(int location) {
+		return new LinkedList<Integer>();
+	}
+	
+	
+}
